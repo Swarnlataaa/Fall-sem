07847f840669c2b1a5b4c8a5dc001df4eefa8cec
SL <82523453+Swarnlataaa@users.noreply.github.com>
2023-01-17 09:07:07 +0530

    Delete turtle directory

--- turtle/1.py
+++
@@ -1,9 +0,0 @@
-import turtle
-s=turtle.getscreen() #initialsing a variable to open a turtle screen
-t=turtle.Turtle() #initialising a variable
-t.right(90)   #t.rt
-t.forward(100)  #t.fd
-t.left(90)  #t.lt
-t.backward(100) #t.bk
-
-#https://realpython.com/beginners-guide-python-turtle/
